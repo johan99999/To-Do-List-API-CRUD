e3c328ffbd29a9c82604d7bd9bbbcb47f50eb126
hanlie <johansama9999@gmail.com>
2024-10-06 10:24:28 +0700

    [START] Menambahkan directory project springboot, dan dependency nya

---
+++ src/main/java/com/johan99999/todolist/ToDoListApiCrudApplication.java
@@ -0,0 +1,13 @@
+package com.johan99999.todolist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToDoListApiCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToDoListApiCrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/johan99999/todolist/ToDoListApiCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.johan99999.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoListApiCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
